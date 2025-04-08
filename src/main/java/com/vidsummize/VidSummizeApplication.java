320177c4f3ff4d1ecc22cb8ea3c14788a9d1b33b
Mateus Merlim Mattos <merlimmateus@gmail.com>
2025-04-08 00:02:39 -0300

    Start Project
    YML file, project scope and OT

---
+++ src/main/java/com/vidsummize/VidSummizeApplication.java
@@ -0,0 +1,13 @@
+package com.vidsummize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidSummizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidSummizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vidsummize/VidSummizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidsummize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidSummizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
